5677912f94168de5d7a57c8f0f6f042dbebd50e9
Ingmar Valerio Piacentini <v.piacentini@nesea.it>
2022-09-14 08:41:51 +0200

    initial commit

---
+++ src/app/hero-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit }        from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'toh-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.scss' ]
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.heroService.getHero(Number(params.get('id'))))
+    ).subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
