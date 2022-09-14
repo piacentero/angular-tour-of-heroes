2e5a7570bad1727246935c184e3ca0db56acb800
Ingmar Valerio Piacentini <v.piacentini@nesea.it>
2022-09-14 14:25:12 +0200

    heroes fixes

--- src/app/features/heroes/heroes.component.ts
+++ src/app/features/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Hero } from '@shared/models/hero';
 import { HeroService } from '@shared/services/hero.service';
-import { finalize, Observable, tap } from 'rxjs';
+import { BehaviorSubject, debounceTime, finalize, Observable, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'toh-heroes',
@@ -15,25 +15,31 @@ export class HeroesComponent implements OnInit {
   lastIdHeroes: number;
   loadingHeroes: boolean = false;
 
+  private refreshHeroes: BehaviorSubject<void> = new BehaviorSubject(null);
+
   constructor(private heroService: HeroService,
               private router: Router) {
   }
 
   ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  getHeroes(): void {
-    this.loadingHeroes = true;
-    this.heroes$ = this.heroService
-        .getHeroes().pipe(
-          tap((heroes)=> {
-            this.lastIdHeroes= heroes[heroes.length-1].id;
-          }),
-          finalize(()=> {
-            this.loadingHeroes = false;
-          })
-        );
+    this.heroes$ = this.refreshHeroes.asObservable().pipe(
+      debounceTime(100),
+      tap(() => this.loadingHeroes = true),
+      switchMap(() => this.heroService.getHeroes().pipe(
+        finalize(() => this.loadingHeroes = false)
+      )),
+      tap((heroes) => this.lastIdHeroes = heroes[heroes.length-1].id)
+    );
+    // this.loadingHeroes = true;
+    // this.heroes$ = this.heroService
+    //     .getHeroes().pipe(
+    //       tap((heroes)=> {
+    //         this.lastIdHeroes= heroes[heroes.length-1].id;
+    //       }),
+    //       finalize(()=> {
+    //         this.loadingHeroes = false;
+    //       })
+    //     );
   }
 
   add(name: string, id: number): void {
@@ -43,7 +49,8 @@ export class HeroesComponent implements OnInit {
     }
     this.heroService.create(name,id)
         .subscribe(hero => {
-          this.getHeroes();
+          // this.getHeroes();
+          this.refreshHeroes.next();
           this.selectedHero = null;
         });
   }
@@ -52,7 +59,8 @@ export class HeroesComponent implements OnInit {
     this.heroService
         .delete(hero.id)
         .subscribe(() => {
-          this.getHeroes();
+          // this.getHeroes();
+          this.refreshHeroes.next();
           if (this.selectedHero === hero) {
             this.selectedHero = null;
           }
